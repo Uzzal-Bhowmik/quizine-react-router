9d0321dadc304626e3a903dc3b1637ee137ae068
Uzzal Bhowmik <ubcreativity111@gmail.com>
2023-06-26 20:58:27 +0600

    loaders added in blogs

--- src/components/DynamicBlog/DynamicBlog.js
+++ src/components/DynamicBlog/DynamicBlog.js
@@ -3,12 +3,13 @@ import "./DynamicBlog.css";
 import { useLoaderData } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { FaArrowLeft } from 'react-icons/fa';
+import { Spinner } from 'react-bootstrap';
 
 
 const DynamicBlog = () => {
     const id = useLoaderData();
-    // const [blogs, setBlogs] = useState([]);
     const [blog, setBlog] = useState({});
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         fetch("/blogs.json")
@@ -16,32 +17,44 @@ const DynamicBlog = () => {
             .then(data => {
                 const blog = data.find(blg => blg.id === id);
                 setBlog(blog);
+
+                if (blog) {
+                    setIsLoading(false)
+                }
             })
     }, [id])
 
 
     return (
         <div className='container mx-auto dyno-blog'>
-            <div className='my-4'>
-                <img src={blog?.img} alt="" style={{ width: "100%", height: "500px" }} />
-            </div>
+            {isLoading ?
+                <Spinner animation="border" variant="warning" className='d-block mt-5 mx-auto fs-2' />
 
-            <div className='blog-desc'>
-                <h1>{blog?.title}</h1>
-                <p className='text-bold text-muted'><small>by {blog?.author}</small></p>
+                :
+                <>
+                    <div className='my-4'>
+                        <img src={blog?.img} alt="" style={{ width: "100%", height: "500px" }} />
+                    </div>
 
-                <p className='px-2'>{blog?.desc}</p>
+                    <div className='blog-desc'>
+                        <h1>{blog?.title}</h1>
+                        <p className='text-bold text-muted'><small>by {blog?.author}</small></p>
 
-                <p className='text-bold text-muted'><small>Last Updated On {blog?.date}</small></p>
+                        <p className='px-2'>{blog?.desc}</p>
 
-                <div>
-                    <span className="arrow-icon text-success"><FaArrowLeft></FaArrowLeft></span>
-                    <Link to={`/blogs`} className='ps-3 pt-3 read-more text-decoration-none  text-success'>
-                        Go Back
-                    </Link>
-                </div>
+                        <p className='text-bold text-muted'><small>Last Updated On {blog?.date}</small></p>
 
-            </div>
+                        <div>
+                            <span className="arrow-icon text-success"><FaArrowLeft></FaArrowLeft></span>
+                            <Link to={`/blogs`} className='ps-3 pt-3 read-more text-decoration-none  text-success'>
+                                Go Back
+                            </Link>
+                        </div>
+
+                    </div>
+                </>
+
+            }
 
 
         </div>
